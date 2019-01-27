e2ae38f6f699bddba4111d6859be3cbcfe891263
Sarah Folk <dobo_n_Christ@yahoo.com>
2019-01-27 20:45:05 +0100

    wrote CRUD

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 'use strict';
-exports.DATABASE_URL = 
-    process.env.DATABASE_URL || 'mongodb://localhost/blog-app-mongoose';
-exports.TEST_DATABASE_URL = 
-    process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app-mongoose';
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app';
 exports.PORT = process.env.PORT || 8080;
